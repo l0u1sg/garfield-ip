4bd5e16f0e80835a2778706ecf231f5960a83cac
Louis <me@louisgallet.fr>
2023-06-27 15:47:30 +0200

    fix: :bug:  Fix ip loading and page name

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   const [ip, setIP] = useState('')
 
   const getData = async () => {
-    const res = await axios.get('https://api.ipify.org?format=json');
-    console.log(res.data.ip)
-    setIP(res.data.ip);
+    const res = await axios.get('https://ifconfig.louisgallet.fr/ip');
+    console.log(res.data)
+    setIP(res.data);
   }
 
   useEffect(() => {
